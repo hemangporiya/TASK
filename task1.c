a1476fbb34db855ff40c532fbc0fc4f4f7be4ce2
hemangporiya <hemangporiya10@gmail.com>
2025-09-09 13:44:45 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n name      :hemang poriya");
+    printf("\n birthdate :7 feb 2001");
+    printf("\n age       :24");
+    printf("\n adress    :zanzarda road junagadh");
+    getch();
+}
---
+++ task2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+     
+    int a = 20;
+    int b = 10;
+    
+    int add = a+b;
+    printf("\n %d", add);
+    
+    int sub = a-b;
+    printf("\n %d", sub);
+    
+    int mul = a*b;
+    printf("\n %d", mul);
+    
+    int div1 = a/b;
+    printf("\n %d", div1);
+    
+    int mod = a%b;
+    printf("\n %d", mod);
+    
+                
+    
+    getch();
+    
+    
+}
